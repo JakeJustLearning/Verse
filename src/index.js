dd3f4a1348278f4fa380499cf9804f6204cd9cf5
Jacob Ross <jacobkarmelross@gmail.com>
2021-09-17 15:14:11 +0200

    feat(added ar initiate and stop session button): added AR start stop button

--- src/index.js
+++ src/index.js
@@ -16,22 +16,37 @@ const renderer = new THREE.WebGLRenderer()
 
 renderer.setSize(window.innerWidth, window.innerHeight)
 
-document.body.appendChild(renderer.domElement)
 
-const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+// const geometry = new THREE.BoxGeometry()
+// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
 
-const cube = new THREE.Mesh(geometry, material)
+// const cube = new THREE.Mesh(geometry, material)
 
-scene.add(cube)
+// scene.add(cube)
 
 camera.position.z = 5
 
+document.body.appendChild(renderer.domElement)
+
+document.body.appendChild(ARButton.createButton(renderer, {
+  optionalFeatures: ['dom-overlay'],
+  domOverlay: {
+    root: document.body
+  }
+}))
+
+renderer.xr.enabled = true
+
 function animate() {
-  requestAnimationFrame(animate)
+  renderer.setAnimationLoop(render)
+}
+function render() {
   renderer.render(scene, camera)
 }
+
 animate()
+
+
 
 
 
