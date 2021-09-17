48fc092e92a743ba892b5b9330fd74b986705d85
Jacob Ross <jacobkarmelross@gmail.com>
2021-09-17 13:29:46 +0200

    docs(added path to home for https git page testing): added home to git page

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,6 +1,16 @@
 import * as THREE from 'three'
+import { ARButton } from 'three/examples/jsm/webxr/ARButton'
 
 
 const scene = new THREE.Scene()
+
+const camera = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth / window.innerHeight,
+  .01,
+  50
+)
+
+document.body.appendChild(ARButton.createButton(renderer))
 
 
