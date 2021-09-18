24b5fceb138c8b55dad3b267bac893fe59955359
Jacob Ross <jacobkarmelross@gmail.com>
2021-09-18 13:06:34 +0200

    fix: rebuild webpack to allow commented out part to work

--- src/index.js
+++ src/index.js
@@ -78,38 +78,38 @@ const init = async () => {
 
 }
 
-const runHitTest = (renderer, timestamp, frame, reticle) => {
-  if (frame) {
-    const referenceSpace = renderer.xr.getReferenceSpace()
-    const session = renderer.xr.getSession()
+// const runHitTest = (renderer, timestamp, frame, reticle) => {
+//   if (frame) {
+//     const referenceSpace = renderer.xr.getReferenceSpace()
+//     const session = renderer.xr.getSession()
 
-    if (hitTestSourceRequested === false) {
-      session.requestReferenceSpace('viewer')
-        .then(referenceSpace => {
-          session.requestHitTestSource({ space: referenceSpace })
-            .then(source => {
-              hitTestSource = source
-            })
-        })
-      session.addEventListener('end', function () {
-        hitTestSourceRequested = false
-        hitTestSource = null
-      })
-      hitTestSourceRequested = true
-    }
-    if (hitTestSource) {
-      const hitTestResults = frame.getHitTestResults(hitTestSource);
-      if (hitTestResults.length) {
-        const hit = hitTestResults[0]
+//     if (hitTestSourceRequested === false) {
+//       session.requestReferenceSpace('viewer')
+//         .then(referenceSpace => {
+//           session.requestHitTestSource({ space: referenceSpace })
+//             .then(source => {
+//               hitTestSource = source
+//             })
+//         })
+//       session.addEventListener('end', function () {
+//         hitTestSourceRequested = false
+//         hitTestSource = null
+//       })
+//       hitTestSourceRequested = true
+//     }
+//     if (hitTestSource) {
+//       const hitTestResults = frame.getHitTestResults(hitTestSource);
+//       if (hitTestResults.length) {
+//         const hit = hitTestResults[0]
 
-        reticle.visible = true
-        reticle.matrix.fromArray(hit.getPose(referenceSpace).transform.matrix)
-      } else {
-        reticle.visbile = false
-      }
-    }
-  }
-}
+//         reticle.visible = true
+//         reticle.matrix.fromArray(hit.getPose(referenceSpace).transform.matrix)
+//       } else {
+//         reticle.visbile = false
+//       }
+//     }
+//   }
+// }
 
 
 function animate() {
